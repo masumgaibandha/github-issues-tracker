fc5e5eef25735efcbca414950b5758971e46c127
masumgaibandha <masumgaibandha@gmail.com>
2026-03-07 12:14:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const signBtn = document.getElementById('sign-btn').addEventListener('click', function(e){
+    e.preventDefault()
+    const userNameInput = document.getElementById('user-name')
+    const userInput = userNameInput.innerText
+    console.log(userInput)
+    const passwordInput = document.getElementById('password')
+})
