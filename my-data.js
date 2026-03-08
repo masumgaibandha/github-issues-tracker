30c0930e37f30979476090bac164675e3d7f609d
masumgaibandha <masumgaibandha@gmail.com>
2026-03-08 14:17:40 +0600

    toggling add

--- my-data.js
+++ my-data.js
@@ -3,55 +3,13 @@
 // let closedBtn = document.getElementById('open-btn');
 
 // allBtn.onclick = () => selectedIssue('all-btn')
-// console.log(first)
 
-// document.getElementById("all-btn").onclick = function () {
-//   console.log('button clicked')
-// };
- let allIssues = []
+document.getElementById("all-btn").onclick = function () {
+  console.log('button clicked')
+};
+
  const allIssuesCard = document.getElementById('issues-card')
  const loadingSpinner = document.getElementById('loading-spinner')
-const openBtn = document.getElementById('open-btn')
-const closedBtn = document.getElementById('closed-btn')
-
-function setActiveBtn(activeBtn){
-    allBtn.classList.remove('bg-[#4A00FF]', 'text-white')
-    openBtn.classList.remove('bg-[#4A00FF]', 'text-white')
-    closedBtn.classList.remove('bg-[#4A00FF]', 'text-white')
-    activeBtn.classList.add('bg-[#4A00FF]', 'text-white')
- }
-
-closedBtn.onclick = function(){
-    const closedIssues = allIssues.filter(clIssue => 
-        clIssue.status.toLowerCase()=== "closed"      
-    )
-    displayAllIssues(closedIssues)
-    totalCounts(closedIssues)
-    setActiveBtn(closedBtn)
-} 
-
- const allBtn = document.getElementById('all-btn')
- const issuesCount = document.getElementById("issues-count")
- 
- allBtn.onclick = function(){
-    displayAllIssues(allIssues)
-    totalCounts(allIssues);
-    setActiveBtn(allBtn)
- }
-
- openBtn.onclick = function () {
-  const openIssues = allIssues.filter(
-    issue => issue.status.toLowerCase() === "open"
-  );
-  displayAllIssues(openIssues);
-  totalCounts(openIssues);
-  setActiveBtn(openBtn)
-};
-  
- 
- function totalCounts(issues){
-    issuesCount.innerText = issues.length; 
- }
 
  function showLoading(){
 loadingSpinner.classList.remove("hidden")
@@ -61,8 +19,6 @@ allIssuesCard.innerHTML = ""
 loadingSpinner.classList.add('hidden')
  }
 
- 
-
 async function loadAllIssue() {
     showLoading()
         const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
@@ -70,19 +26,10 @@ async function loadAllIssue() {
         const data = await res.json()
         hideLoading()
         displayAllIssues(data.data)
-        allIssues = data.data
-        totalCounts(allIssues)
-    allBtn.classList.add('bg-[#4A00FF]', 'text-white')
-    
-    
-       
     }
 
-
     const displayAllIssues = (issues) =>{  
- 
-        // console.log(issues)
-        allIssuesCard.innerHTML = ""   
+        // allIssuesCard.innerHTML = ""   
 
        issues.forEach(issue => {
         // console.log(issue)
@@ -91,7 +38,7 @@ async function loadAllIssue() {
           <div class="card bg-base-100 shadow-2xl border-t-4 border-t-[#00A96E] h-full">
             <div class="card-body">
               <div class="flex justify-between items-center">
-                <div><button class="btn">${issue.status}</button></div>
+                <div>${issue.status}</div>
 
                 <button class="btn bg-[#FEECEC] text-[#EF4444] rounded-[100px] w-20 h-8 p-2 px-5">
                   ${issue.priority}
@@ -116,20 +63,17 @@ async function loadAllIssue() {
         
        });
 
+    // const allBtn = document.getElementById('all-btn').addEventListener('click', function(){
+    //     const issuesCount = document.getElementById('issues-count')
+    //     issuesCount.innerText = allIssuesCard.children.length 
+    // })
+ 
 
- }
-
-     
-
+    }
 loadAllIssue()
-setActiveBtn(allBtn)
 
 
 const createElements = (arr) => {
   const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
   return htmlElements.join(" ");
 };
-
-
-
-    
